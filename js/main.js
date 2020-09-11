b030c1bd203a27da19200288da3dff1668e6425e
Rostyslav Lekh <rostlex2001@gmail.com>
2020-09-11 14:06:03 +0300

    lesson 3

--- js/main.js
+++ js/main.js
@@ -25,6 +25,8 @@ function getQuantityElements(heightElement) {
 }
 function startGame() {
   start.classList.add("hide");
+  gameArea.innerHTML = '';
+  gameArea.classList.remove("hide");
   for (let i = 0; i < getQuantityElements(100); i++) {
     const line = document.createElement("div");
     line.classList.add("line");
@@ -41,14 +43,20 @@ function startGame() {
     enemy.style.background = 'transparent url(../img/enemy2.png) center / cover no-repeat';
     gameArea.appendChild(enemy);
   }
+  setting.score = 0;
   setting.start = true;
   gameArea.appendChild(car);
+  car.style.left = '125px';
+  car.style.top = 'auto'
+  car.style.bottom = '10px';
   setting.x = car.offsetLeft;
   setting.y = car.offsetTop;
   requestAnimationFrame(playGame);
 }
 function playGame() {
   if (setting.start) {
+    setting.score += setting.speed;
+    score.innerHTML = 'Score:<br>' + setting.score;
     moveRoad();
     moveEnemy();
     if (keys.ArrowLeft && setting.x > 0) {
@@ -92,6 +100,17 @@ function moveRoad() {
 function moveEnemy() {
   let enemy = document.querySelectorAll('.enemy');
   enemy.forEach(function (item) {
+    let carRect = car.getBoundingClientRect();
+    let enemyRect = item.getBoundingClientRect();
+
+    if (carRect.top <= enemyRect.bottom &&
+      carRect.right >= enemyRect.left &&
+      carRect.left <= enemyRect.right &&
+      carRect.bottom >= enemyRect.top) {
+      setting.start = false;
+      start.classList.remove('hide');
+      start.style.top = score.offsetHeight + 'px';
+    }
     item.y += setting.speed / 2;
     item.style.top = item.y + 'px';
     if (item.y >= document.documentElement.clientHeight) {
